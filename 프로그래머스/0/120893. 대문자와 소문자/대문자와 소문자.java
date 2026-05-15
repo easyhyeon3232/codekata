86d79b3d77dfd90a6c74d1558a6d0ddec82fa255
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-05-15 10:16:59 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.23 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i =0; i<my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            if(c>='A' && c<= 'Z') {
+                answer.append(Character.toLowerCase(c));
+            } else{
+                answer.append(Character.toUpperCase(c));
+            }
+        }
+        return answer.toString();
+    }
+}
