8d6aca529b20bc69e8d82138a7abcf7a18040a90
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-06-16 22:29:33 +0900

    [level 0] Title: 369게임, Time: 0.10 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int order) {
+        
+        String str = String.valueOf(order).replaceAll("[^369]", "");
+        
+        return str.length();
+    }
+}
